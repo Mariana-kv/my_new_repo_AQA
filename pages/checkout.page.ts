842b7f165a1f914eb476caf5d4d37a46da133948
Mariana Kolesnichenko <mariana@anitechsolutions.com>
2026-05-05 18:28:47 +0300

    Added HW unit 13

--- pages/checkout.page.ts
+++ pages/checkout.page.ts
@@ -5,7 +5,22 @@ export class CheckoutPage {
     productQuantity: Locator;
     productTitle: Locator;
     proceedCheckoutBtn: Locator;
-   
+    productCheckoutPrice: Locator;
+    productCheckoutTotalPrice: Locator;
+    productCheckoutSuccessBtn: Locator;
+    countryCheckoutPage: Locator;
+    postalCodeCheckoutPage: Locator;
+    houseNumberCheckoutPage: Locator;
+    addressCheckoutPageProceedBtn: Locator;
+    cardCheckoutPage: Locator;
+    cardNumberCheckoutPage: Locator;
+    expirationDateCheckoutPage: Locator;
+    cvvCheckoutPage: Locator;
+    cardNameCheckoutPage: Locator;
+    confirmBtnCheckoutPage: Locator;
+    successCheckoutPage: Locator;
+
+
 
 
     constructor(page: Page) {
@@ -13,6 +28,20 @@ export class CheckoutPage {
         this.productQuantity = this.page.getByTestId('product-quantity');
         this.productTitle = this.page.getByTestId('product-title');
         this.proceedCheckoutBtn = this.page.getByTestId('proceed-1');
+        this.productCheckoutPrice = this.page.getByTestId('product-price');
+        this.productCheckoutTotalPrice = this.page.getByTestId('line-price');
+        this.productCheckoutSuccessBtn = this.page.getByTestId('proceed-2');
+        this.countryCheckoutPage = this.page.getByTestId('country');
+        this.postalCodeCheckoutPage = this.page.getByTestId('postal_code');
+        this.houseNumberCheckoutPage = this.page.getByTestId('house_number');
+        this.addressCheckoutPageProceedBtn = this.page.getByTestId('proceed-3');
+        this.cardCheckoutPage = this.page.getByTestId('payment-method');
+        this.cardNumberCheckoutPage = this.page.getByTestId('credit_card_number');
+        this.expirationDateCheckoutPage = this.page.getByTestId('expiration_date')
+        this.cvvCheckoutPage = this.page.getByTestId('cvv');
+        this.cardNameCheckoutPage = this.page.getByTestId('card_holder_name');
+        this.confirmBtnCheckoutPage = this.page.getByTestId('finish');
+        this.successCheckoutPage = this.page.getByTestId('payment-success-message');
 
     }
 }
---
+++ tests/test2.spec.ts
@@ -0,0 +1,128 @@
+
+import { Category } from '../enums/category.enum';
+import { test, expect } from '../fixtures/app.fixture';
+
+
+test('Verify user can view product details', async({ allPages, page }) => {
+
+  await page.goto('/');
+  await allPages.homePage.productLink.click();
+
+  expect(page.url()).toContain('/product');
+  await expect(allPages.productPage.productName).toHaveText('Combination Pliers');
+  await expect(allPages.productPage.unitPrice).toContainText('14.15');
+  await expect(allPages.productPage.addToCart).toBeVisible();
+  await expect(allPages.productPage.addToFav).toBeVisible();
+});
+
+
+test.describe('Verify user can perform sorting by name (asc & desc)', () => {
+  const cases = [
+    { option: 'Name (A - Z)', order: 'asc' },
+    { option: 'Name (Z - A)', order: 'desc'},
+  ];
+
+  for (const { option, order } of cases) {
+    test(`Verify sorting by name: ${option}`, async ({ allPages, page }) => {
+
+      await page.goto('/');
+
+
+      await allPages.homePage.sortDropdown.selectOption(option);
+      const firstProductBefore = await page.getByTestId('product-name').first().textContent();
+await expect(page.getByTestId('product-name').first()).not.toHaveText(firstProductBefore ?? '');
+
+      const names = await page.getByTestId('product-name').allTextContents();
+
+      const sorted = [...names].sort((a, b) =>
+        order === 'asc' ? a.localeCompare(b) : b.localeCompare(a)
+      );
+
+      expect(names).toEqual(sorted);
+    });
+  }
+});
+
+test.describe('Verify user can perform sorting by price (asc & desc)', () => {
+  const cases = [
+    { option: 'Price (Low - High)', order: 'asc' },
+    { option: 'Price (High - Low)', order: 'desc' },
+  ];
+
+  for (const { option, order } of cases) {
+    test(`Verify sorting by price: ${option}`, async ({ allPages, page }) => {
+      await page.goto('/');
+
+      await allPages.homePage.sortDropdown.selectOption(option);
+      const firstProductBefore = await page.getByTestId('product-price').first().textContent();
+await expect(page.getByTestId('product-price').first()).not.toHaveText(firstProductBefore ?? '');
+
+      const priceTexts = await page.getByTestId('product-price').allTextContents();
+
+      const prices = priceTexts.map(p => parseFloat(p.replace('$', '')));
+
+      const sorted = [...prices].sort((a, b) =>
+        order === 'asc' ? a - b : b - a
+      );
+
+      expect(prices).toEqual(sorted);
+    });
+  }
+});
+
+test('Verify user can filter products by category', async({ allPages, page}) => {
+
+  await page.goto('/');
+
+  await allPages.homePage.filterByCategory(Category.PowerTools, 'Sander');
+  await expect(page.getByTestId('product-name').filter({ hasText: 'Combination Pliers' })).toHaveCount(0);
+  const productNames = await allPages.homePage.products.allTextContents();
+  for (const name of productNames) {
+    expect(name).toContain('Sander');
+  }
+});
+
+test('Verify user logged with new user data', async({ loggedInApp, page}) => {
+    await page.goto('/account');
+    await expect(page).toHaveURL('/account');
+    await expect(loggedInApp.accountPage.header.navigationMenu).toContainText('Jack Howe');
+})
+
+test('Verify user can confirm purchase', async({ loggedInApp, page}) => {
+    await page.goto('/');
+    const productName = await loggedInApp.homePage.products.first().textContent();
+    const productPrice = await page.getByTestId('product-price').first().textContent();
+
+    await loggedInApp.homePage.products.first().click();
+    await loggedInApp.productPage.addToCart.click();
+    await expect(loggedInApp.productPage.alert).toContainText('Product added to shopping cart');
+    await expect(loggedInApp.productPage.alert).toBeHidden( {timeout: 8000} );
+    await loggedInApp.productPage.productCart.click();
+    await expect(page).toHaveURL('/checkout');
+    await expect(loggedInApp.checkoutPage.productTitle).toContainText(productName ?? '');
+    await expect(loggedInApp.checkoutPage.productCheckoutPrice).toContainText(productPrice ?? '');
+    await expect(loggedInApp.checkoutPage.productCheckoutTotalPrice).toContainText(productPrice ?? '');
+    await loggedInApp.checkoutPage.proceedCheckoutBtn.click();
+    await expect(page.getByText('Jack Howe, you are already logged in')).toBeVisible();
+    await expect(loggedInApp.checkoutPage.productCheckoutSuccessBtn).toBeVisible();
+    await loggedInApp.checkoutPage.productCheckoutSuccessBtn.click();
+    await loggedInApp.checkoutPage.countryCheckoutPage.selectOption('Ukraine');
+    await loggedInApp.checkoutPage.postalCodeCheckoutPage.fill('46000');
+    await loggedInApp.checkoutPage.houseNumberCheckoutPage.fill('23');
+    await loggedInApp.checkoutPage.addressCheckoutPageProceedBtn.click();
+    await loggedInApp.checkoutPage.cardCheckoutPage.selectOption('Credit Card');
+    await loggedInApp.checkoutPage.cardNumberCheckoutPage.fill('1111-1111-1111-1111');
+
+    const date = new Date();
+    date.setMonth(date.getMonth() + 3);
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = String(date.getFullYear());
+    const expirationDate = `${month}/${year}`;
+    await loggedInApp.checkoutPage.expirationDateCheckoutPage.fill(expirationDate);
+
+    await loggedInApp.checkoutPage.cvvCheckoutPage.fill('111');
+    await loggedInApp.checkoutPage.cardNameCheckoutPage.fill('Mariana K');
+    await loggedInApp.checkoutPage.confirmBtnCheckoutPage.click();
+    await expect (loggedInApp.checkoutPage.successCheckoutPage).toBeVisible();
+    await expect (loggedInApp.checkoutPage.successCheckoutPage).toHaveText('Payment was successful');
+})
