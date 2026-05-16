59b824149548e556f5af4a0f759050da65bb79fc
Mariana Kolesnichenko <mariana@anitechsolutions.com>
2026-05-16 16:37:36 +0300

    debug

--- tests/auth-login.spec.ts
+++ tests/auth-login.spec.ts
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 console.log('EMAIL:', process.env.USER_EMAIL);
 console.log('PASSWORD:', process.env.USER_PASSWORD);
+console.log('BASE_URL:', process.env.BASE_URL);
 import path from 'path';
 
 const authFile = path.join(__dirname, '../playwright/.auth/user.json');
@@ -12,7 +13,7 @@ const authFile = path.join(__dirname, '../playwright/.auth/user.json');
 test('Login and save session', async ({ page }) => {
   const loginPage = new LoginPage(page);
   const accountPage = new AccountPage(page);
-  await page.goto(`${process.env.BASE_URL}/auth/login`);
+  await page.goto('/auth/login');
   await loginPage.performLogin(process.env.USER_EMAIL ?? '', process.env.USER_PASSWORD ?? '');
 
   await expect(page).toHaveURL('/account');
